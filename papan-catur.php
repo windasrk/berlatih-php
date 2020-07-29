9e613ec474e07aa6e3161c2f9118f9ba321455d3
windasrk <windasrk@gmail.com>
2020-07-30 05:14:59 +0700

    pesan perubahannya2

--- papan-catur.php
+++ papan-catur.php
@@ -1,33 +1,67 @@
-<?php
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>PAPAN CATUR</title>
+</head>
+<body>
+	<h2>PAPAN CATUR</h2>
+	<?php
 
-function papancatur($baris) {
-    $x=1;
-    echo "<table align=center border=0 cellspacing=0>";
-    while ($x <= $baris) {
-        echo "<tr>";
-        $y=1;
-        while ($y <= $baris) {
-            if (($x+$y) %2 > 0)
-            echo "<td><br>
-                </td>";
-            else
-            echo "<td>#
-                </td>";
-            $y++;
-        }
-        echo "</tr>";
-        $x++;
-    }
-    echo "</table><br><br>";
-} 
+function papan_catur($angka) {
+// tulis kode di sini
+	echo "Papan Catur ". $angka. "<br>";
+	
+	for ($i=0; $i<$angka; $i++)
+	{
+		for ($h=0;$h<$angka*2-($i%2);$h++)
+		{
+			if ($i%2 == 0 && $h%2 == 0)
+				echo "#";
+			if ($i%2 == 0 && $h%2 == 1)
+				echo "&nbsp";
+			if ($i%2 == 1 && $h%2 == 0)
+				echo "&nbsp";
+			if ($i%2 == 1 && $h%2 == 1)
+				echo "#";
+		}
+                    
+		echo "<br>";
+	}
+	echo "<br><br>";
+}
 
 // TEST CASES
-echo papancatur(4); 
+echo papan_catur(4); 
 /*
 # # # #
  # # #
 # # # #
  # # #
  */
-echo papancatur(5); 
+
+echo papan_catur(8);
+/* 
+# # # # # # # #
+ # # # # # # # 
+# # # # # # # #
+ # # # # # # # 
+# # # # # # # #
+ # # # # # # #
+# # # # # # # #
+ # # # # # # #
+*/
+
+echo papan_catur(5) ;
+/*
+# # # # #
+ # # # #
+# # # # #
+ # # # # 
+# # # # #
+*/
 ?>
+</body>
+</html>
